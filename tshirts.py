ff2ef0992ba184dfaf73c9ba250bd146563d67a6
Mru452001 <muttuh1045@gmail.com>
2025-07-31 11:58:58 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -2,13 +2,13 @@
 def size(cms):
     if cms < 38:
         return 'S'
-    elif cms >= 38 and cms <= 42:
+    elif cms > 38 and cms < 42:
         return 'M'
     else:
         return 'L'
 
-
 assert(size(37) == 'S')
+assert(size(38) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
 print("All is well (maybe!)")
